bf7f4b8e3a055d13b8f5f44ae8c536953e8daf93
Nthnael <carlonathanael53@gmail.com>
2022-12-26 15:38:52 +0700

    Update Progress

--- resources/views/history/detail.blade.php
+++ resources/views/history/detail.blade.php
@@ -4,13 +4,14 @@
     <div class="container">
         <div class="container mt-4 mb-4">
             <div class="col-md-12">
-                <a href="{{ '/' }}" class="btn btn-danger mb-3"><i class="fa fa-arrow-left"></i> Back</a>
+                <a href="{{ '/history' }}" class="btn btn-danger mb-3"><i class="fa fa-arrow-left"></i> Back</a>
             </div>
             <div class="col-md-12">
                 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a class="text-danger text-decoration-none" href="{{ '/' }}">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Checkout</li>
+                        <li class="breadcrumb-item"><a class="text-danger text-decoration-none" href="{{ '/history' }}">History</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Checkout Detail</li>
                     </ol>
                 </nav>
             </div>
